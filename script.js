f82a32c239f40b62a32541f25566dc6ad94a5f42
shaik mohamed faizal <mohamedfaizal813@gmail.com>
2024-08-29 22:01:04 +0530

    first assignment to cleared

---
+++ script.js
@@ -0,0 +1,15 @@
+// Task - 1 Three integers are given in line by line.
+// Task - 2 Print the integers in a single line separate by space.
+
+// initiate the values.
+const a = 2;
+b = 4;
+c = 5;
+
+// print the values in a single line.
+function printTheSingleLine() {
+  console.log(a + " " + b + " " + c);
+}
+
+// call the function.
+printTheSingleLine();
